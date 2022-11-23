8c3812a5ae3572c07d9c1c40f75ad5f6036fda2b
apfox500 <apfox500@gmail.com>
2022-11-22 17:34:11 -0700

    modal sheet

--- perfect_plate/lib/home.dart
+++ perfect_plate/lib/home.dart
@@ -36,8 +36,7 @@ class _HomePageState extends State<HomePage> {
               //Colorie display
               Container(
                 decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primary,
-                    borderRadius: BorderRadius.circular(5)),
+                    color: Theme.of(context).colorScheme.primary, borderRadius: BorderRadius.circular(5)),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Text(
@@ -78,73 +77,75 @@ class _HomePageState extends State<HomePage> {
         onPressed: () {
           final nameController = TextEditingController();
 
-          showDialog(
+          showModalBottomSheet(
               context: context,
               builder: (context) {
-                return SimpleDialog(
-                  children: (foodOptions.isEmpty)
-                      ? [
-                          TextField(
-                            controller: nameController,
-                            decoration: const InputDecoration(
-                              label: Text("Name of food"),
-                              hintText: "ex. carrots",
-                            ),
-                            onSubmitted: (name) async {
-                              //TODO fugure out best way for URL
-                              String url =
-                                  "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${global.usdaKey}&query=${name}&dataType=Branded&dataType=Foundation&sortBy=publishedDate&sortOrder=desc";
-                              //So this basically returns most recent 50
-                              //it took like 2 hours to get the url to work, please don't break
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: (foodOptions.isEmpty)
+                        ? [
+                            TextField(
+                              controller: nameController,
+                              decoration: const InputDecoration(
+                                label: Text("Name of food"),
+                                hintText: "ex. carrots",
+                              ),
+                              onSubmitted: (name) async {
+                                //TODO figure out best way for URL
+                                String url =
+                                    "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${global.usdaKey}&query=${name}&dataType=Branded&dataType=Foundation&sortBy=publishedDate&sortOrder=desc";
+                                //So this basically returns most recent 50
+                                //it took like 2 hours to get the url to work, please don't break
 
-                              http.Response response = await http.get(Uri.parse(url));
-                              //just gets the foods from theat json mess
-                              Iterable l = json.decode(response.body)["foods"];
-                              //maps them into a list of foods
-                              foodOptions = List<Food>.from(l.map((model) => Food.fromJson(model)));
-                              //now to clean up duplicates
-                              for (int i = 0; i < foodOptions.length; i++) {
-                                try {
-                                  String company = foodOptions[i].brandName!;
-                                  String name = foodOptions[i].name;
-                                  for (int j = i + 1; j < foodOptions.length; j++) {
-                                    if (foodOptions[j].name == name &&
-                                        foodOptions[j].brandName! == company) {
-                                      foodOptions.removeAt(j);
+                                http.Response response = await http.get(Uri.parse(url));
+                                //just gets the foods from theat json mess
+                                Iterable l = json.decode(response.body)["foods"];
+                                //maps them into a list of foods
+                                foodOptions = List<Food>.from(l.map((model) => Food.fromJson(model)));
+                                //now to clean up duplicates
+                                for (int i = 0; i < foodOptions.length; i++) {
+                                  try {
+                                    String company = foodOptions[i].brandName!;
+                                    String name = foodOptions[i].name;
+                                    for (int j = i + 1; j < foodOptions.length; j++) {
+                                      if (foodOptions[j].name == name && foodOptions[j].brandName! == company) {
+                                        foodOptions.removeAt(j);
+                                      }
                                     }
+                                  } catch (_) {
+                                    print(_);
                                   }
-                                } catch (_) {
-                                  print(_);
                                 }
-                              }
-                              //Then to display it
-                              setState(() {});
-                            },
-                          ),
-                        ]
-                      : [
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height * 9,
-                            width: MediaQuery.of(context).size.width * 95,
-                            child: ListView.builder(
-                                itemCount: foodOptions.length,
-                                itemBuilder: (context, index) {
-                                  return Card(
-                                    child: Column(
-                                      children: [
-                                        Text(
-                                          foodOptions[index].name,
-                                          style: Theme.of(context).textTheme.bodyMedium,
-                                        ),
-                                        Text(
-                                          foodOptions[index].toString(),
-                                        ),
-                                      ],
-                                    ),
-                                  );
-                                }),
-                          ),
-                        ],
+                                //Then to display it
+                                setState(() {});
+                              },
+                            ),
+                          ]
+                        : [
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * .5,
+                              width: MediaQuery.of(context).size.width * .95,
+                              child: ListView.builder(
+                                  itemCount: foodOptions.length,
+                                  itemBuilder: (context, index) {
+                                    return Card(
+                                      child: Column(
+                                        children: [
+                                          Text(
+                                            foodOptions[index].name,
+                                            style: Theme.of(context).textTheme.bodyMedium,
+                                          ),
+                                          Text(
+                                            foodOptions[index].simple4(),
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  }),
+                            ),
+                          ],
+                  ),
                 );
               });
         },
